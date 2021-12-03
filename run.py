54d3b3b07945bcb390d69f304744e5edc7053658
Didisimmons <onyema.onyejekwe@gmail.com>
2021-12-03 22:48:01 +0000

    Deployment to Heroku

--- run.py
+++ run.py
@@ -1,29 +1,51 @@
 import os
-from flask import Flask, render_template
+import json
+from flask import Flask, render_template, request, flash
+if os.path.exists("env.py"):
+    import env
 
 
-app = Flask(__name__) # this creates an instance and The first argument of the Flask class, is the name of the application's module - our package.This informs Flask  where to look for templates and static files.
+app = Flask(__name__)
+app.secret_key = os.environ.get("SECRET_KEY")
 
-
-@app.route("/")# route decorator tells Flask what URL should trigger the function that follows. The "/" indicates browse the root directory that triggers the inndex function 
+@app.route("/")
 def index():
     return render_template("index.html")
 
 
 @app.route("/about")
 def about():
-    return render_template("about.html")
+    data = []
+    with open ("data/company.json","r") as json_data:
+        data = json.load(json_data) 
+    return render_template("about.html", page_title = "About", company = data) 
 
 
-@app.route("/contact")
+
+@app.route("/about/<member_name>")
+def about_member(member_name):
+    member = {}
+    with open ("data/company.json","r") as json_data:
+       data = json.load(json_data)
+       for obj in data:
+          if obj["url"] == member_name: 
+              member = obj
+    return render_template("member.html", member = member)
+
+
+
+
+@app.route("/contact", methods=["GET", "POST"])
 def contact():
-    return render_template("contact.html")
+    if request.method == "POST":
+        flash("Thanks {}, we have received your message!".format(
+            request.form.get("name")))
+    return render_template("contact.html", page_title="Contact")
 
 
 @app.route("/careers")
 def careers():
-    return render_template("careers.html")
-
+    return render_template("careers.html",  page_title="Careers")
 
 if __name__ == "__main__":
     app.run(
